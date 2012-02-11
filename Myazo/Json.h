b4c791ca2ce2c46094829d50ce3bf9031046babe
abcsharp <^p^>
2012-02-11 18:22:39 +0900

    8.5th commit.

--- Myazo/Json.h
+++ Myazo/Json.h
@@ -1,4 +1,4 @@
-#include "ShareHeaders.h"
+ #include "ShareHeaders.h"
 
 
 namespace Json
@@ -26,13 +26,8 @@ namespace Json
 		std::shared_ptr<double> Double_Value;
 		std::shared_ptr<bool> Bool_Value;
 		std::shared_ptr<std::wstring> String_Value;
-<<<<<<< HEAD
 		std::shared_ptr<Json::Hash> Hash_Value;
 		std::shared_ptr<Json::Array> Array_Value;
-=======
-		std::shared_ptr<Hash> Hash_Value;
-		std::shared_ptr<Array> Array_Value;
->>>>>>> origin/master
 		Type Type_Value;
 		bool IsNull_Value;
 		
@@ -45,24 +40,14 @@ namespace Json
 		Item(const double& Double);
 		Item(const bool& Bool);
 		Item(const String& String);
-<<<<<<< HEAD
 		Item(const Json::Hash& Hash);
 		Item(const Json::Array& Array);
-=======
-		Item(const Hash& Hash);
-		Item(const Array& Array);
->>>>>>> origin/master
 		Item(long long&& Int);
 		Item(double&& Double);
 		Item(bool&& Bool);
 		Item(String&& String);
-<<<<<<< HEAD
 		Item(Json::Hash&& Hash);
 		Item(Json::Array&& Array);
-=======
-		Item(Hash&& Hash);
-		Item(Array&& Array);
->>>>>>> origin/master
 		~Item(void);
 		Type Type(void)const;
 		bool IsNull(void)const;
@@ -73,17 +58,11 @@ namespace Json
 		bool& Bool(void);
 		const bool& Bool(void)const;
 		std::wstring& String(void);
-<<<<<<< HEAD
 		const std::wstring& String(void)const;
 		Json::Hash& Hash(void);
 		const Json::Hash& Hash(void)const;
 		Json::Array& Array(void);
 		const Json::Array& Array(void)const;
-
-=======
-		Hash& Hash(void);
-		Array& Array(void);
->>>>>>> origin/master
 		Item& operator=(const Item& LeftRef);
 		Item& operator=(Item&& RightRef);
 
